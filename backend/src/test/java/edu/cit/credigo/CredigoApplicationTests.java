a68620d8be7f7f7b93c4ace148b4457204626f0c
LAB13_USER ON WS38 <L13X13W38@citulab.net>
2025-02-12 13:30:12 +0800

    add backend

---
+++ backend/src/test/java/edu/cit/credigo/CredigoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.credigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
