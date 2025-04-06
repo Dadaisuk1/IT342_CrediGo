71d8aa2cb8735a7f90b4f526bb05cbfeeb0c315d
MutsarelaCheese <Darwindarryljean.largoza@gmail.com>
2025-04-06 19:32:49 +0800

    home page

--- frontend_web/src/pages/Home.jsx
+++ frontend_web/src/pages/Home.jsx
@@ -1,8 +1,29 @@
 import React, { useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Home = () => {
+
+    const location = useLocation();
+    const navigate = useNavigate();
+    const isSignIn = location.pathname === '/sign-in';
+    const isSignUp = location.pathname === '/sign-up';
+
+    const handleNavigation = (path) => {
+        navigate(path);
+    };
+
     return (
-        <p className='Flex text-[32px] text-[#FFFFFE]'>Welcome Home!</p>
+        <div className='flex justify-center'>
+            <p className='Flex text-[32px] text-[#FFFFFE]'>Welcome Home!</p>
+            <button
+                className='p-5 text-[32px] text-[#FFFFFE]'
+                onClick={() => handleNavigation('/sign-in')}
+            >Sign In</button>
+            <button
+                className='p-5 text-[32px] text-[#FFFFFE]'
+                onClick={() => handleNavigation('/sign-up')}
+            >Sign Up</button>
+        </div>
     );
 };
 
