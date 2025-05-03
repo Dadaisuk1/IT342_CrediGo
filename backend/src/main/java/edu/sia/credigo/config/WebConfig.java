5caa901d79c5a45896b981577fa905dd3088a8c6
Jake Luis Lood <jakeluis.lood@cit.edu>
2025-05-03 17:57:07 +0800

    Success maybe

--- backend/src/main/java/edu/sia/credigo/config/WebConfig.java
+++ backend/src/main/java/edu/sia/credigo/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://it-342-credi-go.vercel.app/") // <== use pattern instead of .allowedOrigins
+                .allowedOriginPatterns("https://it-342-credi-go.vercel.app") // <== use pattern instead of .allowedOrigins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
